ba956f28bdd37cddbcc7ec926176ee3225fdb6da
Maxime <maxime_hemmerle@orange.fr>
2020-03-16 10:06:57 +0100

    test SDK fb

---
+++ App.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Alert, Button, Image, TextInput, AppRegistry } from 'react-native';
+
+var FBLoginButton = require('./FBLoginButton');
+
+class Hello extends Component {
+	render() {
+		return (
+			<View style={{fontSize: 16}}>
+				<Text> Helol{this.props.name}!</Text>
+			</View>
+		);
+	}
+}
+
+export default class MyApp extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = { text: 'Default text'};
+	}
+
+	render() {
+		return (
+			// VIEW = BODY (Tout doit être dedans)
+			// button = BOUTON (method onPress = action au click)
+			// Image = Images (lien ou source)
+	    	<View style={styles.container}>
+					<Text style={styles.label}>Welcome to the Facebook SDK for React Native!</Text>
+					<FBLoginButton />
+					<Hello name="Maxime"/>
+		      <Text id="montext" style={{fontSize: 20}}>{this.state.text}</Text>
+					<Button
+	        title="Press me"
+	        onPress={() => Alert.alert('jai appuyé')}
+	      />
+				<Image
+          style={{width: 100, height: 100}}
+          source={{uri: 'https://cdn.pixabay.com/photo/2019/08/21/08/33/nose-4420535_960_720.jpg'}}
+        />
+				<TextInput
+					id="test"
+	        style={{height: 40, width: '80%', borderColor: 'gray', borderWidth: 1}}
+	        onChangeText={(text) => this.setState({text})}
+      	/>
+	    </View>
+	  );
+	}
+}
+
+AppRegistry.registerComponent('MyApp', () => UselessTextInput);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#F5FCFF',
+	  },
+	  label: {
+	    fontSize: 16,
+	    fontWeight: 'normal',
+	    marginBottom: 48,
+	  },
+});
